09da72ab606579ac1dfef8f5315032c7ad36b31f
alex george <alexandreantoinegeorge@gmail.com>
2018-11-19 17:39:12 -0500

    controllers and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    resources :favours, only: [:index, :create] do
+      resources :responds, only: [:index, :create]
+  end
+    resources :informations, only: [:index, :create] do
+      resources :responds, only: [:index, :create]
+  end
+    resources :events, only: [:index, :create] do
+      resources :responds, only: [:index, :create]
+  end
 end
