4d5d82e6ac0c7e9b2803d7c318b676933afb8a83
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-20 16:28:53 -0500

    saving progress

--- app/assets/javascripts/unhide_forms.js
+++ app/assets/javascripts/unhide_forms.js
@@ -1,5 +1,5 @@
 function unhideForm(formType) {
-    var form_div = document.getElementById(formType);
+    const form_div = document.getElementById(formType);
     if (form_div.style.display === "none") {
         form_div.style.display = "block";
     } else {
