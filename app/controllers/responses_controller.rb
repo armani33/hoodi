482842f32ef6d23d416a5f91a709d8a08ddc2247
alex george <alexandreantoinegeorge@gmail.com>
2018-11-22 11:54:24 -0500

    response create methode added - view HTML) added for test

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,24 @@
 class ResponsesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @response = response.new(response_params)
+    @response.post = @post
+    if @response.save
+      respond_to do |format|
+        format.html { redirect_to #home ?? }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render #home ?? }
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:content)
+  end
 end
