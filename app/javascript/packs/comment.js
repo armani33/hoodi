e06e139ea4e008170db7820dc5989f08c58122d3
alex george <alexandreantoinegeorge@gmail.com>
2018-11-23 16:04:01 -0500

    hidden class for responses added on comment (only for favour)

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -1,11 +1,21 @@
-const buttonComment = document.querySelector("#comment");
+// const buttonComment = document.querySelector("#comment");
+// buttonComment.addEventListener("click", (event) => {
+// const post_window = document.getElementById('response');
+// favourHTML = "test"
+// event.currentTarget.classList.toggle("hidden-content");
+// // favourHTML = "<% if favour.responses.blank? %>Leave a response<% else %><%favour.responses.each do |response_object|%><%= render('responses/show', { response: response_object }) %><% end %><% end %>";
+// // post_window.insertAdjacentHTML('afterend', favourHTML);
+
+// event.preventDefault();
+// });
+
+
+//
+const response = document.querySelector(".response");
+const buttonComment = document.querySelector(".comment-button")
+// console.log(buttonComment);
 buttonComment.addEventListener("click", (event) => {
-  const post_window = document.getElementById('response');
-favourHTML = "<% if favour.responses.blank? %>Leave a response<% else %><%favour.responses.each do |response_object|%><%= render('responses/show', { response: response_object }) %><% end %><% end %>";
-post_window.insertAdjacentHTML('afterend', favourHTML);
-event.preventDefault();
-
-});
-
-
+    event.preventDefault();
+response.classList.toggle("hidden");
+  });
 
