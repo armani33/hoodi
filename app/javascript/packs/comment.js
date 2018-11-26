7bce62b4ee26f626d98e01571a08a4da0fa613ac
alex george <alexandreantoinegeorge@gmail.com>
2018-11-26 17:29:57 -0500

    comment.js final update

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -1,16 +1,3 @@
-// const buttonComment = document.querySelector("#comment");
-// buttonComment.addEventListener("click", (event) => {
-// const post_window = document.getElementById('response');
-// favourHTML = "test"
-// event.currentTarget.classList.toggle("hidden-content");
-// // favourHTML = "<% if favour.responses.blank? %>Leave a response<% else %><%favour.responses.each do |response_object|%><%= render('responses/show', { response: response_object }) %><% end %><% end %>";
-// // post_window.insertAdjacentHTML('afterend', favourHTML);
-
-// event.preventDefault();
-// });
-
-
-//
 // Selection for favour
 if (document.querySelector(".response")!= null) {
 const responseFavour = document.querySelector(".response");
@@ -18,8 +5,6 @@ const buttonCommentFavour = document.querySelector(".comment-button");
 const commentContentFavour = document.querySelector("#comment");
 toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
 }
-// toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
-// toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
 // Selection for Information
 else if (document.querySelector(".response-info")!= null) {
 const responseInfo = document.querySelector(".response-info");
@@ -34,11 +19,6 @@ const buttonCommentEvent = document.querySelector(".comment-button-event")
 const commentContentEvent = document.querySelector("#comment-event")
 toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
 }
-
-// toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
-// toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
-// toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
-
 function toggleResponses(button, response, comment) {
   button.addEventListener("click", (event) => {
     event.preventDefault();
@@ -46,15 +26,3 @@ response.classList.toggle("hidden");
 comment.classList.toggle("underline")
   });
 }
-
-
-
-// const responseInfo = document.querySelector(".response-info");
-// const buttonCommentInfo = document.querySelector(".comment-button-info")
-// const commentContentInfo = document.querySelector("#comment-info")
-// console.log(buttonComment);
-// buttonCommentInfo.addEventListener("click", (event) => {
-//     event.preventDefault();
-// responseInfo.classList.toggle("hidden");
-// commentContentInfo.classList.toggle("underline")
-//   });
