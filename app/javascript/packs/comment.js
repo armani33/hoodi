669b75ba1aefe2c0fa7471894ed2db57c1c3cdbd
alex george <alexandreantoinegeorge@gmail.com>
2018-11-26 17:24:39 -0500

    comment.js updated

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -12,21 +12,32 @@
 
 //
 // Selection for favour
+if (document.querySelector(".response")!= null) {
 const responseFavour = document.querySelector(".response");
 const buttonCommentFavour = document.querySelector(".comment-button");
 const commentContentFavour = document.querySelector("#comment");
+toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
+}
+// toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
+// toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
 // Selection for Information
+else if (document.querySelector(".response-info")!= null) {
 const responseInfo = document.querySelector(".response-info");
 const buttonCommentInfo = document.querySelector(".comment-button-info");
 const commentContentInfo = document.querySelector("#comment-info");
+toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
+}
 // Selection for Information
+else {
 const responseEvent = document.querySelector(".response-event");
 const buttonCommentEvent = document.querySelector(".comment-button-event")
 const commentContentEvent = document.querySelector("#comment-event")
-
-toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
-toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
 toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
+}
+
+// toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
+// toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
+// toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
 
 function toggleResponses(button, response, comment) {
   button.addEventListener("click", (event) => {
