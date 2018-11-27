b0745a00007ed15440ad9fdf69a7731f6f8c0501
alex george <alexandreantoinegeorge@gmail.com>
2018-11-27 13:43:56 -0500

    When click on message, response hidden and message form displayed

--- app/javascript/packs/message.js
+++ app/javascript/packs/message.js
@@ -1,8 +1,16 @@
 document.querySelector(".message-button").addEventListener("click", (event) => {
   event.preventDefault();
-
+// if (document.querySelector(".response")!=null) {
+  document.querySelector(".response").classList.add("hidden");
+  document.querySelector("#comment").classList.remove("underline");
+// }
 const messageFavour = document.querySelector(".new-message");
 messageFavour.classList.toggle("hidden");
 });
 
 
+
+
+// document.querySelector(".response hidden")
+
+// document.querySelector(".response")
