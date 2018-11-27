b37326b2c80750515bfcb01836caf6a7829de701
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-27 18:34:24 -0500

    user can rsvp to an event

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'reservations/create'
   devise_for :users
   root to: 'pages#home'
   get 'pages/nearby_users', to: 'pages#nearby_users'
     resources :favourites, only: [:index]
+    resources :reservations, only: [:index]
     resources :responses, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -15,6 +18,7 @@ Rails.application.routes.draw do
     end
 
     resources :events, only: [:index, :create] do
+      resources :reservations, only: [:create]
       resources :responses, only: [:index, :create]
     end
 
