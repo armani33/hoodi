d6d90dcb16049bf2bb254f011963fb11daa13c6b
alex george <alexandreantoinegeorge@gmail.com>
2018-11-29 16:51:41 -0500

    help button works for all cards

--- app/javascript/packs/message.js
+++ app/javascript/packs/message.js
@@ -1,9 +1,11 @@
-document.querySelector(".message-button").addEventListener("click", (event) => {
+const blueCards = document.querySelectorAll(".blue-card");
+
+blueCards.forEach((blueCard) => {
+blueCard.querySelector(".message-button").addEventListener("click", (event) => {
   event.preventDefault();
-  document.querySelector(".response").classList.add("hidden");
-  document.querySelector("#comment").classList.remove("underline");
-const messageFavour = document.querySelector(".new-message");
+  blueCard.querySelector(".response").classList.add("hidden");
+  blueCard.querySelector("#comment").classList.remove("underline");
+const messageFavour = blueCard.querySelector(".new-message");
 messageFavour.classList.toggle("hidden");
 });
-
-
+});
