9fc5e491b7a3d54cb388ff6ee234aa9087a19ad1
alex george <alexandreantoinegeorge@gmail.com>
2018-11-29 17:03:54 -0500

    comment js updated with all cards

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -1,10 +1,13 @@
 // Selection for favour
-if (document.querySelector(".response")!= null) {
-const responseFavour = document.querySelector(".response");
-const buttonCommentFavour = document.querySelector(".comment-button");
-const commentContentFavour = document.querySelector("#comment");
+const blueCards = document.querySelectorAll(".blue-card");
+
+blueCards.forEach((blueCard) => {
+
+const responseFavour = blueCard.querySelector(".response");
+const buttonCommentFavour = blueCard.querySelector(".comment-button");
+const commentContentFavour = blueCard.querySelector("#comment");
 toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
-}
+});
 // Selection for Information
 // else if (document.querySelector(".response-info")!= null) {
 // const responseInfo = document.querySelector(".response-info");
@@ -13,12 +16,12 @@ toggleResponses(buttonCommentFavour, responseFavour, commentContentFavour);
 // toggleResponses(buttonCommentInfo, responseInfo, commentContentInfo);
 // }
 // Selection for Information
-else {
-const responseEvent = document.querySelector(".response-event");
-const buttonCommentEvent = document.querySelector(".comment-button-event")
-const commentContentEvent = document.querySelector("#comment-event")
-toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
-}
+// else {
+// const responseEvent = document.querySelector(".response-event");
+// const buttonCommentEvent = document.querySelector(".comment-button-event")
+// const commentContentEvent = document.querySelector("#comment-event")
+// toggleResponses(buttonCommentEvent, responseEvent, commentContentEvent);
+// }
 function toggleResponses(button, response, comment) {
   button.addEventListener("click", (event) => {
     event.preventDefault();
@@ -26,4 +29,4 @@ document.querySelector(".new-message").classList.add("hidden");
 response.classList.toggle("hidden");
 comment.classList.toggle("underline");
   });
-}
+};
