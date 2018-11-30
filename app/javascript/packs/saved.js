433644912841bcfe69aff2e5f56207d6c88b8b8a
alex george <alexandreantoinegeorge@gmail.com>
2018-11-30 10:49:20 -0500

    refresh on comment added

--- app/javascript/packs/saved.js
+++ app/javascript/packs/saved.js
@@ -1,8 +1,9 @@
 const neutral = document.querySelector('.not-saved');
 const saved = document.querySelector('.saved');
 
-document.querySelector(".footer-link-save").addEventListener("click", (event) => {
+document.querySelector(".save-post-link").addEventListener("click", (event) => {
 
+event.preventDefault();
 
   if (neutral.classList.contains('hidden')){
     document.querySelector(".not-saved").classList.remove("hidden");
