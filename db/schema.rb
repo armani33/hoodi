e46c9e6a3ac556b0b6d9088ead1706cdcd754628
alex george <alexandreantoinegeorge@gmail.com>
2018-11-30 07:20:50 -0500

    countdown ok ToDo: rework on event creation date not good format

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_000950) do
+ActiveRecord::Schema.define(version: 2018_11_30_115540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
